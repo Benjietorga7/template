869f52a2ab43d15b435ce93c24f02acb3542519b
Benjietorga7 <Benjietorga7@gmail.com>
2023-01-28 18:43:00 +0800

    updated

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -19,10 +19,19 @@
          }
          .gallery img{
             max-width: 100%;
+            max-height:100%;
+            padding: 5px;
+            background: white;
+            box-shadow: 0px 0px 15px rgba(0,0,0,0.3);
+            cursor: pointer;
          }
          .gallery{
             padding: 80px 0px;
          }
+         .gallery h3{
+            margin-bottom: 25px;
+            color: gray;
+         } 
 
         </style>
 
@@ -50,7 +59,7 @@
                             <div class="d-flex h-100">
                                 <div class="project-text w-80 my-auto text-right text-lg-left">
                                     
-                                    <p class="mb-0 text-white-50 text left">Rey Rose Construction and supply is a “One-Stop Shop” for high quality homebuilding hardware and construction materials, paints, home furnishing and a complete line of hardware items and accessories. It is now on its 34th year of dedicated service to its patrons and customers which include contractors, engineers, architects, interior designers, real estate developers, institutions and homeowners    </p>
+                                    <p class="mb-0 text-white-50 texts">Rey Rose Construction and supply is a “One-Stop Shop” for high quality homebuilding hardware and construction materials, paints, home furnishing and a complete line of hardware items and accessories. It is now on its 34th year of dedicated service to its patrons and customers which include contractors, engineers, architects, interior designers, real estate developers, institutions and homeowners    </p>
                                     <hr class="d-none d-lg-block mb-0 ms-0" />
                                 </div>
                             </div>
@@ -79,53 +88,86 @@
         <!-- Projects-->
         <section class="about-section text-center bg-gray" id="projects">
             <div class="container px-4 px-lg-5">
-                <!-- Featured Project Row-->
+                <!-- Service-->
                 <div class="row gx-0 mb-4 mb-lg-5 align-items-center">
-                    <div class="col-xl-8 col-lg-7"><img class="img-fluid mb-3 mb-lg-0" src="assets/img/qwe.jpg" alt="..." /></div>
+                    <div class="col-xl-8 col-lg-7"><img class="img-fluid mb-3 mb-lg-0" src="assets/img/eq1.jpg" alt="..." /></div>
                     <div class="col-xl-4 col-lg-5">
                         <div class="featured-text text-center text-lg-left">
-                            <h4>Shoreline</h4>
-                            <p class="text-light-50 mb-0">Grayscale is open source and MIT licensed. This means you can use it for any project - even commercial projects! Download it, customize it, and publish your website!</p>
+                            <h4 class="text-white-50">Service</h4>
+                            <p class="mb-0 text-white-50">Our company offers free delivery of materials that has been bought from our company, We do also offer electrical works, mechanical works, structural works, material & equipment supply, inspection, planning, design, logistics, and the like.</p>
                         </div>
                     </div>
                 </div>
-                <!-- Project One Row-->
+                <!-- gallery-->
                 <section class="gallery">
+                    <h3 >Here are the list of our Products</h3>
                     <div class="container-lg">
-                        <div class="row gy-4 row-cols-1 row-cols-sm-2 row-cols-md-3">
+                        <div class="row gy-5 row-cols-1 row-cols-sm-2 row-cols-md-3">
+                            
                             <div class="col">
-                                <img src="assets/img/q2.jpg" class="gallery-item" alt="Gallery1">
+                                <img src="assets/img/tool.jpg" class="gallery-item" alt="Gallery1">
+                                <p class="text-white">complete set of tools</p>
                             </div>
                             <div class="col">
-                                <img src="assets/img/q2.jpg" class="gallery-item" alt="Gallery1">
+                                <img src="assets/img/semento.jpg" class="gallery-item" alt="Gallery1">
+                                <p class="text-white">Cements</p>
                             </div>
                             <div class="col">
-                                <img src="assets/img/q2.jpg" class="gallery-item" alt="Gallery1">
+                                 <img src="assets/img/ko.jpg" class="gallery-item" alt="Gallery1">
+                                <p class="text-white">Heavy equipments</p>
                             </div>
                             <div class="col">
-                                <img src="assets/img/q2.jpg" class="gallery-item" alt="Gallery1">
+                                <img src="assets/img/tiles.jpg" class="gallery-item" alt="Gallery1">
+                                <p class="text-white">floor tiles</p>
                             </div>
                             <div class="col">
-                                <img src="assets/img/q2.jpg" class="gallery-item" alt="Gallery1">
+                                <img src="assets/img/puthaw.jpg" class="gallery-item" alt="Gallery1">
+                                <p class="text-white">iron rod</p>
                             </div>
                             <div class="col">
-                                <img src="assets/img/q2.jpg" class="gallery-item" alt="Gallery1">
+                                <img src="assets/img/bas.jpg" class="gallery-item" alt="Gallery1">
+                                <p class="text-white">sand dumps</p>
                             </div>
+                            <div class="col">
+                                <img src="assets/img/rocks.jpg" class="gallery-item" alt="Gallery1">
+                                <p class="text-white">rock pebbles</p>
+                            </div>
+                            <div class="col">
+                                <img src="assets/img/pipe.jpg" class="gallery-item" alt="Gallery1">
+                                <p class="text-white">pipe</p>
+                            </div>
+                            <div class="col">
+                                <img src="assets/img/roofs.jpg" class="gallery-item" alt="Gallery1">
+                                <p class="text-white">Variety of Roofs</p>
+                            </div>
+                           
                         </div>
                     </div>
                 
                 </section>
+                
+               <div class="modal fade" id="gallery-popup" tabinex="-1" aria-labelledby="exampleModalLabel"aria-hidden="true">
+                    <div class="modal-dialog">
+                        <div class="modal-content">
+                            <div class="modal-header">
+                                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"> </button>
+                                    <div class="modal-body">
+                                        <img src="assets/img/q2.jpg" class="modal-img" alt="Modal Image">
+                                    </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
                
                 <!-- Project Two Row-->
                 <div class="row gx-0 justify-content-center">
-                    <div class="col-lg-6"><img class="img-fluid" src="assets/img/demo-image-02.jpg" alt="..." /></div>
-                    <div class="col-lg-6 order-lg-first">
+                  
+                     <div class="col-lg-6 order-lg-first">
                         <div class="bg-black text-center h-100 project">
                             <div class="d-flex h-100">
                                 <div class="project-text w-100 my-auto text-center text-lg-right">
-                                    <h4 class="text-white">Mountains</h4>
-                                    <p class="mb-0 text-white-50">Another example of a project with its respective description. These sections work well responsively as well, try this theme on a small screen!</p>
-                                    <hr class="d-none d-lg-block mb-0 me-0" />
+                                   
+                                    
                                 </div>
                             </div>
                         </div>
@@ -159,19 +201,7 @@
                             <!---->
                             <!-- This is what your users will see when the form-->
                             <!-- has successfully submitted-->
-                            <div class="d-none" id="submitSuccessMessage">
-                                <div class="text-center mb-3 mt-2 text-white">
-                                    <div class="fw-bolder">Form submission successful!</div>
-                                    To activate this form, sign up at
-                                    <br />
-                                    <a href="https://startbootstrap.com/solution/contact-forms">https://startbootstrap.com/solution/contact-forms</a>
-                                </div>
-                            </div>
-                            <!-- Submit error message-->
-                            <!---->
-                            <!-- This is what your users will see when there is-->
-                            <!-- an error submitting the form-->
-                            <div class="d-none" id="submitErrorMessage"><div class="text-center text-danger mb-3 mt-2">Error sending message!</div></div>
+                            
                         </form>
                     </div>
                 </div>
@@ -187,7 +217,7 @@
                                 <i class="fas fa-map-marked-alt text-primary mb-2"></i>
                                 <h4 class="text-uppercase m-0">Address</h4>
                                 <hr class="my-4 mx-auto" />
-                                <div class="small text-black-50">4923 Market Street, Orlando FL</div>
+                                <div class="small text-black-50"><a href="https://www.google.com/maps/place/Rey+Rose+Construction+and+Supply/@8.956078,125.5799941,17z/data=!4m5!3m4!1s0x3301ead3e9d33801:0x79ca49d34a61a9c!8m2!3d8.9560844!4d125.5822134" target="https://www.google.com/maps/place/Rey+Rose+Construction+and+Supply/@8.956078,125.5799941,17z/data=!4m5!3m4!1s0x3301ead3e9d33801:0x79ca49d34a61a9c!8m2!3d8.9560844!4d125.5822134">4923 Market Street, Orlando FL</a></div>
                             </div>
                         </div>
                     </div>
@@ -197,7 +227,7 @@
                                 <i class="fas fa-envelope text-primary mb-2"></i>
                                 <h4 class="text-uppercase m-0">Email</h4>
                                 <hr class="my-4 mx-auto" />
-                                <div class="small text-black-50"><a href="#!">hello@yourdomain.com</a></div>
+                                <div class="small text-black-50"><p>hello@yourdomain.com</p></div>
                             </div>
                         </div>
                     </div>
@@ -213,10 +243,11 @@
                     </div>
                 </div>
                 <div class="social d-flex justify-content-center">
-                    <a class="mx-2" href="#!"><i class="fab fa-twitter"></i></a>
-                    <a class="mx-2" href="#!"><i class="fab fa-facebook-f"></i></a>
-                    <a class="mx-2" href="#!"><i class="fab fa-github"></i></a>
+                    
+                    <a class="mx-2" href="https://www.facebook.com/pages/Rey-Rose-Construction-Supply/656232811244186" target="https://www.facebook.com/pages/Rey-Rose-Construction-Supply/656232811244186"><i class="fab fa-facebook-f"></i></a>
+                    
                 </div>
             </div>
         </section>
+      
     @endsection
